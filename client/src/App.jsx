69a7a6ce24813e8e364d2592187c0cf153027450
Alex V <al.voytovsky@gmail.com>
2020-08-08 02:00:53 +0300

    feat: all the necessary settings and configurations add to start development

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './App.sass'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          New Application!
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default App
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,24 +3,56 @@ const webpack = require('webpack')
 
 module.exports = {
   entry: {
-    app: './index.js',
+    app: './index.jsx',
   },
-  context: path.resolve(__dirname, 'static_src'),
+  context: path.resolve(__dirname, 'src'),
   output: {
-    path: path.resolve(__dirname, 'static', 'build'),
+    path: path.resolve(__dirname, 'public', 'dist'),
     filename: 'app.js',
+    publicPath: '/public/dist/'
   },
+  // mode: 'development',
+  watch: process.argv[process.argv.length - 1] === 'development',
+  devtool: 'cheap-inline-module-source-map',
   module: {
     rules: [
       {
+        enforce: 'pre',
         test: /\.(js|jsx)$/,
-        include: path.resolve(__dirname, 'static_src'),
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
         loader: 'babel-loader',
         exclude: /node_modules/,
-        options: {
-          presets: ['@babel/env'],
-        }
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: [
+            ["@babel/plugin-proposal-class-properties", { "loose": true }]
+          ]
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
+  },
+  // For import without .jsx
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: {
+      index: './public/index.html'
+    },
+    port: 8082,
   },
 }
