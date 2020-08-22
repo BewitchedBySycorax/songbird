503a5bc89251367a804b23ce64c5d87f0b806112
Alex V <al.voytovsky@gmail.com>
2020-08-22 16:17:05 +0300

    feat: add new dynamic routing logic

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import AnswerOptions from './components/AnswerOptions'
+import PropTypes from 'prop-types'
 import Question from './components/Question/Question'
+import AnswerOptions from './components/AnswerOptions'
+import Description from './components/Description'
 
 export default class Router extends Component {
+  static propTypes = {
+    currentStageNumber: PropTypes.number.isRequired
+  }
+
   render() {
+    const { currentStageNumber } = this.props
+    let currentStagePath = null
+    
+    switch (currentStageNumber) {
+      case 1:
+        currentStagePath = '/01_warm_up/'
+        break
+      case 2:
+        currentStagePath = '/02_passerines/'
+        break
+      case 3:
+        currentStagePath = '/03_forest_birds/'
+        break
+      case 4:
+        currentStagePath = '/04_songbirds/'
+        break
+      case 5:
+        currentStagePath = '/05_raptors/'
+        break
+      case 6:
+        currentStagePath = '/06_marine_birds/'
+        break
+      default:
+        break
+    }
+    
     return (
       <Switch>
-        <Route exact path='/01_warm_up/' components={{ question: Question, answerOptions: AnswerOptions }} />
-        <Route exact path='/02_passerines/' components={{ question: Question, answerOptions: AnswerOptions }} />
-        <Route exact path='/03_forest_birds/' components={{ question: Question, answerOptions: AnswerOptions }} />
-        <Route exact path='/04_songbirds/' components={{ question: Question, answerOptions: AnswerOptions }} />
-        <Route exact path='/05_raptors/' components={{ question: Question, answerOptions: AnswerOptions }} />
-        <Route exact path='/06_marine_birds/' components={{ question: Question, answerOptions: AnswerOptions }} />
+        <Route
+          exact path={currentStagePath} 
+          render={props =>
+            <div className='centralContent'>
+              <Question />
+              <div className='question-answerOptions'>
+                <AnswerOptions />
+                <Description />
+              </div>
+            </div>
+          }
+        />
 
-        <Redirect to='/01_warm_up/' />
+        <Redirect to={currentStagePath} />
       </Switch>
     )
   }
