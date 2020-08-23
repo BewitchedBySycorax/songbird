c6b87a75d4747ee7d3f1fe3b3bb34964c64d5065
Alex V <al.voytovsky@gmail.com>
2020-08-23 22:52:43 +0300

    fix: minor formatting changes

--- birdsData.js
+++ birdsData.js
@@ -1,5 +1,5 @@
 const birdsData = [
-  [       
+  [
     {
       id: 1,
       name: 'Ворон',
@@ -265,7 +265,7 @@ const birdsData = [
       description: 'Особенностью голубоногой олуши является не только насыщенный ярко-синий цвет лапок, но еще и тот факт, что они очень теплые. В то время как другие виды птиц греют кладки своим телом, голубоногая олуша делает это с помощью лапок',
       image: 'https://live.staticflickr.com/800/40645471394_4422e69ed8.jpg',
       audio: 'https://www.xeno-canto.org/sounds/uploaded/YHKQPPJDVP/XC411507-171217_1491%20BF%20Booby%205ft%20IDLP%201230%20mp3%20amp.mp3'
-    },    
+    },
     {
       id: 3,
       name: 'Буревестник',
@@ -299,6 +299,6 @@ const birdsData = [
       audio: 'https://www.xeno-canto.org/sounds/uploaded/VOLIQOYWKG/XC501190-190801_06.50h_zilvermeeuw_duinen%20van%20goeree_roep_2ex_overvliegend_gezien_.mp3'
     }
   ]
-];
+]
 
 export default birdsData
